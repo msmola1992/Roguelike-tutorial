2badc4a553bd2ad4ceb52736079c59b264079bff
Bucktoothgamer <msmola1992@hotmail.com>
2022-07-25 20:10:49 -0400

    Engine - Step 4
    Provides light and dark tiles based on players current field of view

--- engine.py
+++ engine.py
@@ -1,7 +1,8 @@
-from typing import Set, Iterable, Any
+from typing import Iterable, Any
 
 from tcod.context import Context
 from tcod.console import Console
+from tcod.map import compute_fov
 
 from entity import Entity  
 from input_handlers import EventHandler
@@ -9,11 +10,11 @@ from game_map import GameMap
 
 
 class Engine:
-    def __init__(self, entities: set[Entity], event_handler: EventHandler, game_map: GameMap, player: Entity):
-        self.entities = entities
+    def __init__(self, event_handler: EventHandler, game_map: GameMap, player: Entity):
         self.even_handler = event_handler
         self.game_map = game_map
         self.player = player
+        self.update_fov()
 
     def handle_events(self, events: Iterable[any]) -> None:
         for event in events:
@@ -23,12 +24,23 @@ class Engine:
                 continue
 
             action.perform(self, self.player)
+
+            self.update_fov() # update FOV before the players next action
     
+    def update_fov(self) -> None:
+        # Recompute visible area based on players point of view
+
+        self.game_map.visible[:] = compute_fov(
+            self.game_map.tiles["transparent"], 
+            (self.player.x, self.player.y),
+            radius = 8, 
+        )
+        # if a tile is visible it should be added to explored
+        self.game_map.explored |= self.game_map.visible
+
     def render(self, console: Console, context: Context) -> None:
         self.game_map.render(console)
 
-        for entity in self.entities:
-            console.print(entity.x, entity.y, entity.char, fg= entity.color)
 
         context.present(console)
 
